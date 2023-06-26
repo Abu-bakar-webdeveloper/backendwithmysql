4aae58084a8b71ad3d8ffb2a3dddebe593255254
abubkar <abubakaraw8@gmail.com>
2023-06-26 14:21:22 +0500

    fix db connection issue

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errormiddleware')
-const connectDB = require('./config/db')
+// const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
+
+// connectDB()
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect('mongodb+srv://abubakar:T2qz6yJYxBDFnBQt@crudcluster.eyywlsk.mongodb.net/mernapp?retryWrites=true&w=majority')
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
 
 connectDB()
 
