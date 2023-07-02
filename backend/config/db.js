bf7556e417aa8231932346dc3d2f97e186b6ff8f
abubkar <abubakaraw8@gmail.com>
2023-07-02 22:46:21 +0500

    changing database to mysql first step connecting db compelete

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,23 @@
-const mongoose = require('mongoose')
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize(
+    'crudapi',
+    'root',
+    '',
+    {
+        dialect: 'mysql',
+        host: 'localhost'
+    }
+);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://abubakar:T2qz6yJYxBDFnBQt@crudcluster.eyywlsk.mongodb.net/mernapp?retryWrites=true&w=majority')
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-    } catch (error) {
+        await sequelize.authenticate();
+        console.log("succesfully connected to our db")
+     } catch (error) {
         console.log(error)
         process.exit(1)
     }
 }
 
-module.exports = connectDB
+module.exports = {sequelize, connectDB}
